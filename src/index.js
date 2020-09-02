382afa206dec476dcad73aa7a3061637c6f464b2
wlad999 <wlad25888@gmail.com>
2020-09-02 18:50:39 +0300

    121 action.payload

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const reducer = (state = 0, action) => {
             return state + 1;
         case 'DEC':
             return state - 1;
+        case 'RND':
+            return state + action.payload;
         default:
             return state
 
@@ -15,9 +17,19 @@ const store = createStore(reducer)
 document.getElementById('inc').addEventListener('click', () => {
     store.dispatch({type: 'INC'})
 })
+
 document.getElementById('dec').addEventListener('click', () => {
     store.dispatch({type: 'DEC'})
 })
+document.getElementById('rnd').addEventListener('click', () => {
+    const payload = Math.floor(Math.random() * 10)
+    store.dispatch({
+        type: 'RND',
+        payload
+    })
+})
+
+
 const update = () => {
     document.getElementById('counter').innerHTML = store.getState()
 }
